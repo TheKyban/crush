8f01b32bcb6167ab2421a78f49aac2863dd8ce07
aditya <aaditya1392@gmail.com>
2023-05-09 20:04:06 +0530

    add functionality for mobile but have bugs

--- script.js
+++ script.js
@@ -1,49 +1,83 @@
 let highestZ = 1;
 
 class Paper {
+    constructor() {
+       this.holdingPaper = false;
+    
+       this.prevMouseX = 0;
+       this.prevMouseY = 0;
+    
+       this.mouseX = 0;
+       this.mouseY = 0;
+    
+       this.velocityX = 0;
+       this.velocityY = 0;
+    
+       this.currentPaperX = 0;
+       this.currentPaperY = 0;
+    
+       this.events = {
+            mouse: {
+                down: "mousedown",
+                up: "mouseup",
+                move: "mousemove"
+            },
+            touch: {
+                down: "touchstart",
+                up: "touchend",
+                move: "touchmove"
+    
+            }
+        }
+    
+        this.deviceType = "";
+        this.isTouchDevice()
+    }
 
-    holdingPaper = false;
-
-    prevMouseX = 0;
-    prevMouseY = 0;
-
-    mouseX = 0;
-    mouseY = 0;
-
-    velocityX = 0;
-    velocityY = 0;
-
-    currentPaperX = 0;
-    currentPaperY = 0;
+    isTouchDevice() {
+        try {
+            document.createEvent('TouchEvent')
+            this.deviceType = "touch"
+            return true;
+        } catch (error) {
+            this.deviceType = "mouse"
+            return false;
+        }
+    }
 
     init(paper) {
-        paper.addEventListener('mousedown', (e) => {
-            // console.log("mouse is selected")
+        paper.addEventListener(this.events[this.deviceType].down, (e) => {
+
+
             this.holdingPaper = true;
             paper.style.zIndex = highestZ;
             highestZ += 1;
 
             if (e.button === 0) {
-                this.prevMouseX = this.mouseX
-                this.prevMouseY = this.mouseY
+                this.prevMouseX = this.isTouchDevice() ? e.touches[0].clientX : e.clientX
+                this.prevMouseY = this.isTouchDevice() ? e.touches[0].clientX : e.clientY
 
-
-                console.log(this.prevMouseX)
-                console.log(this.prevMouseY)
 
             }
         })
 
-        document.addEventListener('mousemove', (e) => {
-            // console.log("mouse is moving")
 
-            this.mouseX = e.clientX
-            this.mouseY = e.clientY
+
+        document.addEventListener(this.events[this.deviceType].move, (e) => {
+
+
+            // this.mouseX = e.clientX
+            // this.mouseY = e.clientY
+
+
+            this.mouseX = this.isTouchDevice() ? e.touches[0].clientX : e.clientX
+
+            this.mouseY = this.isTouchDevice() ? e.touches[0].clientY : e.clientY
 
             this.velocityX = this.mouseX - this.prevMouseX
             this.velocityY = this.mouseY - this.prevMouseY
 
-            if(this.holdingPaper) {
+            if (this.holdingPaper) {
                 this.currentPaperX += this.velocityX
                 this.currentPaperY += this.velocityY
 
@@ -54,7 +88,9 @@ class Paper {
             }
         })
 
-        window.addEventListener('mouseup', (e) => {
+        window.addEventListener(this.events[this.deviceType].up, (e) => {
+
+
             // console.log("mouse btn is released")
 
             this.holdingPaper = false
